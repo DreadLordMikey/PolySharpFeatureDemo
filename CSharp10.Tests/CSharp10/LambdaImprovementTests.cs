af3e64518827289cbc4cc37d889f50c51b55aa51
Michael Hofer <kchighland@hotmail.com>
2023-01-21 13:07:14 -0500

    Constant interpolated strings. Sealed record ToString override and PrintMembers.

--- CSharp10.Tests/CSharp10/LambdaImprovementTests.cs
+++ CSharp10.Tests/CSharp10/LambdaImprovementTests.cs
@@ -1,92 +1,91 @@
 ﻿using System.Linq.Expressions;
 
-namespace PolySharpFeatureTests.CSharp10
+namespace PolySharpFeatureTests.CSharp10;
+
+[TestClass]
+public class LambdaImprovementTests
 {
-    [TestClass]
-    public class LambdaImprovementTests
+    [TestMethod]
+    public void Lambda_OfFunc_IsFuncOfStringInt()
     {
-        [TestMethod]
-        public void Lambda_OfFunc_IsFuncOfStringInt()
-        {
-            // Arrange
-            var parse = (string s) => int.Parse(s);
-            var expected = typeof(Func<string, int>);
+        // Arrange
+        var parse = (string s) => int.Parse(s);
+        var expected = typeof(Func<string, int>);
 
-            // Act
-            var actual = parse.GetType();
+        // Act
+        var actual = parse.GetType();
 
-            // Assert
-            Assert.AreEqual(expected, actual);
-        }
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
 
-        [TestMethod]
-        public void Object_OfFunc_IsFuncOfStringInt()
-        {
-            // Arrange
-            object parse = (string s) => int.Parse(s);
-            var expected = typeof(Func<string, int>);
+    [TestMethod]
+    public void Object_OfFunc_IsFuncOfStringInt()
+    {
+        // Arrange
+        object parse = (string s) => int.Parse(s);
+        var expected = typeof(Func<string, int>);
 
-            // Act
-            var actual = parse.GetType();
+        // Act
+        var actual = parse.GetType();
 
-            // Assert
-            Assert.AreEqual(expected, actual);
-        }
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
 
-        [TestMethod]
-        public void Delegate_OfFunc_IsFuncOfStringInt()
-        {
-            // Arrange
-            Delegate parse = (string s) => int.Parse(s);
-            var expected = typeof(Func<string, int>);
+    [TestMethod]
+    public void Delegate_OfFunc_IsFuncOfStringInt()
+    {
+        // Arrange
+        Delegate parse = (string s) => int.Parse(s);
+        var expected = typeof(Func<string, int>);
 
-            // Act
-            var actual = parse.GetType();
+        // Act
+        var actual = parse.GetType();
 
-            // Assert
-            Assert.AreEqual(expected, actual);
-        }
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
 
-        [TestMethod]
-        public void LambdaExpression_OfFunc_IsFuncOfStringInt()
-        {
-            // Arrange
-            LambdaExpression parseExpr = (string s) => int.Parse(s); // Expression<Func<string, int>>
-            var expected = typeof(Expression<Func<string, int>>);
+    [TestMethod]
+    public void LambdaExpression_OfFunc_IsFuncOfStringInt()
+    {
+        // Arrange
+        LambdaExpression parseExpr = (string s) => int.Parse(s); // Expression<Func<string, int>>
+        var expected = typeof(Expression<Func<string, int>>);
 
-            // Act
-            var actual = parseExpr.GetType();
+        // Act
+        var actual = parseExpr.GetType();
 
-            // Assert
-            Assert.AreEqual(expected, actual);
-        }
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
 
-        [TestMethod]
-        public void Expression_OfFunc_IsFuncOfStringInt()
-        {
-            // Arrange
-            Expression parseExpr = (string s) => int.Parse(s);       // Expression<Func<string, int>>
-            var expected = typeof(Expression<Func<string, int>>);
+    [TestMethod]
+    public void Expression_OfFunc_IsFuncOfStringInt()
+    {
+        // Arrange
+        Expression parseExpr = (string s) => int.Parse(s);       // Expression<Func<string, int>>
+        var expected = typeof(Expression<Func<string, int>>);
 
-            // Act
-            var actual = parseExpr.GetType();
+        // Act
+        var actual = parseExpr.GetType();
 
-            // Assert
-            Assert.AreEqual(expected, actual);
-        }
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
 
-        [TestMethod]
-        public void Lambda_WithReturnType_ReturnsExpectedValue()
-        {
-            // Arrange
-            var choose = object (bool b) => b ? 1 : "two";
+    [TestMethod]
+    public void Lambda_WithReturnType_ReturnsExpectedValue()
+    {
+        // Arrange
+        var choose = object (bool b) => b ? 1 : "two";
 
-            // Act
-            var actual = choose(false);
+        // Act
+        var actual = choose(false);
 
-            // Assert
-            Assert.IsInstanceOfType<string>(actual);
-            Assert.AreEqual("two", actual);
-        }
+        // Assert
+        Assert.IsInstanceOfType<string>(actual);
+        Assert.AreEqual("two", actual);
     }
 }
---
+++ CSharp10.Tests/CSharp10/SealedRecordToStringTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace PolySharpFeatureTests.CSharp10;
+
+[TestClass]
+public class SealedRecordToStringTests
+{
+    [TestMethod]
+    public void Record_WithOverriddenToString_ReturnsExpectedValue()
+    {
+        // Arrange
+        var sut = new Point(5, 10);
+        var expected = "Point.X = 5; Point.Y = 10";
+
+        // Act
+        var actual = sut.ToString();
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Record_WithPrintMembersImpl_ReturnsExpectedValue()
+    {
+        // Arrange
+        var sut = new Rect(5, 10);
+        var expected = "Rect { X: 5, Y: 10 }";
+
+        // Act
+        var actual = sut.ToString();
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+
+    abstract record Shape()
+    {
+        protected virtual bool PrintMembers(StringBuilder stringBuilder) { return false; }
+    }
+
+    private record Rect(int X, int Y) : Shape
+    {
+        protected override bool PrintMembers(StringBuilder stringBuilder)
+        {
+            stringBuilder.Append($"X: {X}, Y: {Y}");
+            return true;
+        }
+    }
+
+    private record Point(int X, int Y)
+    {
+        public override string ToString()
+        {
+            return $"Point.X = {X}; Point.Y = {Y}";
+        }
+    }
+
+}
