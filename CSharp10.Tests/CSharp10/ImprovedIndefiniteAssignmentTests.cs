87572d367fd13860b69db3ea250cbb768a79a8c5
Michael Hofer <34105576+DreadLordMikey@users.noreply.github.com>
2023-01-21 16:49:27 -0500

    Issue 12: Improved indefinite assignments. (#13)

---
+++ CSharp10.Tests/CSharp10/ImprovedIndefiniteAssignmentTests.cs
@@ -0,0 +1,68 @@
+﻿namespace PolySharpFeatureTests.CSharp10;
+
+[TestClass]
+public class ImprovedIndefiniteAssignmentTests
+{
+    [TestMethod]
+    public void Assigment_FromOutParameter_DoesNotProduceFalsePositive()
+    {
+        // Arrange
+        var expected = "Foo";
+        var actual = default(string);
+        var c = new Foo();
+
+        // Act
+        if (c != null && c.GetDependentValue(out object buffer))
+        {
+            actual = buffer.ToString();
+        }
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Assigment_FromNullConditionalOperator_DoesNotProduceFalsePositive()
+    {
+        // Arrange
+        var expected = "Foo";
+        var actual = default(string);
+        var c = new Foo();
+
+        // Act
+        if (c?.GetDependentValue(out object buffer) == true)
+        {
+            actual = buffer.ToString();
+        }
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Assigment_FromNullCoalescingOperator_DoesNotProduceFalsePositive()
+    {
+        // Arrange
+        var expected = "Foo";
+        var actual = default(string);
+        var c = new Foo();
+
+        // Act
+        if (c?.GetDependentValue(out object buffer) ?? false)
+        {
+            actual = buffer.ToString();
+        }
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    private class Foo
+    {
+        public bool GetDependentValue(out object obj)
+        {
+            obj = "Foo";
+            return true;
+        }
+    }
+}
