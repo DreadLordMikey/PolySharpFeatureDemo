54182348c77eed7005166e89ed8a6663265d33d6
Michael Hofer <kchighland@hotmail.com>
2023-04-09 14:08:08 -0400

    #24 File local types.

---
+++ CSharp11.Tests/Helpers/FileLocalTypesImplA.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharp11.Tests.Helpers;
+
+internal class FileLocalTypesImplA
+{
+    public string Name { get => new FileLocalType().Name; }
+}
+
+file class FileLocalType
+{
+    public string Name { get => "FileLocalTypesImplA"; }
+}
---
+++ CSharp11.Tests/UnitTests/FileLocalTypesTests.cs
@@ -0,0 +1,35 @@
+﻿using CSharp11.Tests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp11.Tests.UnitTests;
+
+[TestClass]
+public class FileLocalTypesTests
+{
+
+    [TestMethod]
+    public void Given_FileTypeA_ReturnsCorrectLocalFileName()
+    {
+        // Arrange
+        var sut = new FileLocalTypesImplA();
+
+        // Act
+        var actual = sut.Name;
+
+        // Assert
+        Assert.AreEqual("FileLocalTypesImplA", actual);
+    }
+
+    [TestMethod]
+    public void Given_FileTypeB_ReturnsCorrectLocalFileName()
+    {
+        // Arrange
+        var sut = new FileLocalTypesImplB();
+
+        // Act
+        var actual = sut.Name;
+
+        // Assert
+        Assert.AreEqual("FileLocalTypesImplB", actual);
+    }
+}
