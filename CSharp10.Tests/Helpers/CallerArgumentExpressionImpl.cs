d90ff24a1a761943954f7cf4910e97f63726fa2d
Michael Hofer <kchighland@hotmail.com>
2023-04-09 12:37:04 -0400

    #16 Caller Argument Expressions.

---
+++ CSharp10.Tests/Helpers/CallerArgumentExpressionImpl.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CSharp10.Tests.Helpers
+{
+    internal class CallerArgumentExpressionImpl
+    {
+#pragma warning disable IDE0060 // Remove unused parameter
+        public string Execute(bool condition, [CallerArgumentExpression(nameof(condition))] string expression = null)
+#pragma warning restore IDE0060 // Remove unused parameter
+        {
+            return expression;
+        }
+    }
+}
--- CSharp10.Tests/UnitTests/ExtendedPropertyPatternTests.cs
+++ CSharp10.Tests/UnitTests/ExtendedPropertyPatternTests.cs
@@ -1,4 +1,4 @@
-﻿namespace PolySharpFeatureTests.CSharp10;
+﻿namespace CSharp10.Tests.UnitTests;
 
 [TestClass]
 public class ExtendedPropertyPatternTests
@@ -21,13 +21,17 @@ public class ExtendedPropertyPatternTests
 
     private class Foo
     {
+#pragma warning disable IDE0090 // Use 'new(...)'
         public Bar Bar { get => new Bar(); }
+#pragma warning restore IDE0090 // Use 'new(...)'
         public string Value = "Foo";
     }
 
     private class Bar
     {
+#pragma warning disable IDE0090 // Use 'new(...)'
         public Baz Baz { get => new Baz(); }
+#pragma warning restore IDE0090 // Use 'new(...)'
         public string Value = "Bar";
     }
 
